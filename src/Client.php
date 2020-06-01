386a9bf3d6579c1658dd613fd38c3389d38ccf5f
Memo Chou <memochou1993@gmail.com>
2020-06-01 22:01:10 +0800

    Initial commit

---
+++ src/Client.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace MemoChou1993\Localize;
+
+class Client
+{
+    //
+}
---
+++ tests/ClientTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MemoChou1993\Localize\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ClientTest extends TestCase
+{
+    //
+}
